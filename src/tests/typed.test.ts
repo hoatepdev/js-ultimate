43f99e19fbc37a72926650a02478d5793c52e4b6
Hoà T. Nguyen <hoanguyentrandev@gmail.com>
2024-01-03 20:56:28 +0700

    Feat: define test function for utils isFunction

--- src/tests/typed.test.ts
+++ src/tests/typed.test.ts
@@ -62,4 +62,52 @@ describe('typed module', () => {
       expect(_.isObject({})).toEqual(true)
     })
   })
+
+  describe('isFunction function', () => {
+    test('returns false for null', () => {
+      expect(_.isFunction(null)).toBe(false)
+    })
+    test('returns false for undefined', () => {
+      expect(_.isFunction(undefined)).toBe(false)
+    })
+    test('returns false for boolean', () => {
+      expect(_.isFunction(true)).toBe(false)
+    })
+    test('returns false for class instance', () => {
+      class Data {}
+      expect(_.isFunction(new Data())).toBe(false)
+    })
+    test('returns false for number', () => {
+      expect(_.isFunction(888)).toBe(false)
+    })
+    test('returns false for string', () => {
+      expect(_.isFunction('string')).toBe(false)
+    })
+    test('returns false for array', () => {
+      expect(_.isFunction([])).toBe(false)
+    })
+    test('returns false for object', () => {
+      expect(_.isFunction({})).toBe(false)
+    })
+    test('returns true for anonymous function', () => {
+      expect(
+        _.isFunction(function () {
+          return 'hello'
+        })
+      ).toBe(true)
+    })
+    test('returns true for arrow function', () => {
+      expect(
+        _.isFunction(() => {
+          return 'hello'
+        })
+      ).toBe(true)
+    })
+    test('returns true for named function', () => {
+      function sayHello() {
+        return 'hello'
+      }
+      expect(_.isFunction(sayHello)).toBe(true)
+    })
+  })
 })
