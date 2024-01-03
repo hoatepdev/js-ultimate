89cb63e4d9d4b9e297a6c8b72ec709367d1b9bb1
Hoà T. Nguyen <hoanguyentrandev@gmail.com>
2024-01-03 19:59:21 +0700

    Feat: add test file for function isArray

--- src/tests/typed.test.ts
+++ src/tests/typed.test.ts
@@ -2,8 +2,33 @@ import * as _ from '../typed'
 
 describe('typed module', () => {
   describe('isArray function', () => {
-    test('return null', () => {
+    test('return false for null', () => {
       expect(_.isArray(null)).toBe(false)
+    })
+    test('return false for undefined', () => {
+      expect(_.isArray(undefined)).toBe(false)
+    })
+    test('return false for boolean', () => {
+      expect(_.isArray(true)).toBe(false)
+    })
+    test('return false for object', () => {
+      expect(_.isArray({})).toBe(false)
+    })
+    test('return false for class', () => {
+      class Data {}
+      expect(_.isArray(new Data())).toBe(false)
+    })
+    test('return false for number', () => {
+      expect(_.isArray(68)).toBe(false)
+    })
+    test('return false for string', () => {
+      expect(_.isArray('blabla')).toBe(false)
+    })
+    test('return true for array', () => {
+      expect(_.isArray([1, 2, 3])).toBe(true)
+    })
+    test('return true for empty array', () => {
+      expect(_.isArray([])).toBe(true)
     })
   })
 })
